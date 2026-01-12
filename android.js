a48d7dd19eb391dc6fd5e9d28e99c5d3de0125b3
tsunami9921 <ebilovekber99@gmail.com>
2026-01-13 03:59:55 +0400

    Create android.js

---
+++ android.js
@@ -0,0 +1,29 @@
+/* android.js */
+
+/* ANDROID TOUCH CONTROLS */
+const Android = {
+  initTouchControls: function(){
+    const joystick = document.getElementById("joystick");
+    const jumpBtn = document.getElementById("jumpBtn");
+
+    if(!joystick || !jumpBtn) return;
+
+    // Joystick hareketi
+    joystick.addEventListener("touchmove", e=>{
+      const t = e.touches[0];
+      Joystick.x = (t.clientX - joystick.offsetLeft - joystick.offsetWidth/2)/50;
+      Joystick.y = (t.clientY - joystick.offsetTop - joystick.offsetHeight/2)/50;
+    });
+
+    joystick.addEventListener("touchend", e=>{
+      Joystick.x = 0;
+      Joystick.y = 0;
+    });
+
+    // Jump button
+    jumpBtn.addEventListener("touchstart", ()=>{
+      Joystick.jump = true;
+      setTimeout(()=>Joystick.jump=false,200);
+    });
+  }
+};
